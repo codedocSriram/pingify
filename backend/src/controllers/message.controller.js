8b84fe0e13d596415f33866f7ecc5c25520aec58
Sriram Annadurai <mailsriram98@gmail.com>
2026-01-24 23:28:16 +0530

    Added sendMessage controller logic which will handle /send endpoint

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import Message from "../models/message.model.js";
+
 export const getUsersForSidebar = async (req, res) => {
     try {
         const loggedInUserId = req.user_id;
@@ -9,7 +10,7 @@ export const getUsersForSidebar = async (req, res) => {
 
         res.status(200).json(filteredUsers);
     } catch (error) {
-        console.log("Error in getUsersForSidebar: ", error.message);
+        console.log("Error in getUsersForSidebar controller: ", error.message);
         res.status(500).json({ error: "Internal server error" });
     }
 };
@@ -27,7 +28,30 @@ export const getMessages = async (req, res) => {
         });
         res.status(200).json(messages);
     } catch (error) {
-        console.log("Error in getMessages router: ", error.message);
+        console.log("Error in getMessages contorller: ", error.message);
+        res.status(500).json({ error: "Internal server error" });
+    }
+};
+
+export const sendMessage = async (req, res) => {
+    try {
+        const { id: recieverId } = req.params;
+        const { text, image } = req.body;
+        const senderId = req.user._id;
+        let imageUrl;
+        if (image) {
+            const uploadResponse = await cloudinary.uploader.upload(image);
+            imageUrl = uploadResponse.secure_url;
+        }
+        const newMessage = new Message({
+            senderId,
+            recieverId,
+            text,
+            image: imageUrl,
+        });
+        await newMessage.save();
+    } catch (error) {
+        console.log("Error in sendMessage controller: ", error.message);
         res.status(500).json({ error: "Internal server error" });
     }
 };

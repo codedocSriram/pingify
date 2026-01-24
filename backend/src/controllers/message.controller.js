7e8ff73b435c1c638b64993a4809ac23dcefae8b
Sriram Annadurai <mailsriram98@gmail.com>
2026-01-24 20:06:37 +0530

    added getUsersForSideBar controller and getMessages controller

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,4 +1,20 @@
-export const getUsersForSidebar = (req, res) => {
+import User from "../models/user.model";
+
+export const getUsersForSidebar = async (req, res) => {
+    try {
+        const loggedInUserId = req.user_id;
+        const filteredUsers = await User.find({
+            _id: { $ne: loggedInUserId },
+        }).select("-password");
+
+        res.status(200).json(filteredUsers);
+    } catch (error) {
+        console.error("Error in getUsersForSidebar: ", error.message);
+        res.status(500).json({ error: "Internal server error" });
+    }
+};
+
+export const getMessages = async (req, res) => {
     try {
     } catch (error) {}
 };
--- backend/src/routes/message.router.js
+++ backend/src/routes/message.router.js
@@ -1,8 +1,12 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getUsersForSidebar } from "../controllers/message.controller.js";
+import {
+    getUsersForSidebar,
+    getMessages,
+} from "../controllers/message.controller.js";
 const messageRouter = express.Router();
 
 messageRouter.get("/users", protectRoute, getUsersForSidebar);
+messageRouter.get("/:id", protectRoute, getMessages);
 
 export default messageRouter;

2bcf3739fc68aca015d2a260125f6d654ce6677d
Sriram Annadurai <mailsriram98@gmail.com>
2026-01-24 19:10:31 +0530

    added a checkAuth controller to check whether the user is Authenticated or not

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -142,3 +142,15 @@ export const updateProfile = async (req, res) => {
         });
     }
 };
+
+export const checkAuth = (req, res) => {
+    try {
+        res.status(200).json(req.user);
+    } catch (error) {
+        console.log("Error occured in checkAuth controller", error.message);
+        res.status(500).json({
+            success: false,
+            message: "Internal Server error",
+        });
+    }
+};
--- backend/src/routes/auth.router.js
+++ backend/src/routes/auth.router.js
@@ -4,6 +4,7 @@ import {
     loginController,
     logoutController,
     updateProfile,
+    checkAuth,
 } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 const authRouter = express.Router();
@@ -12,4 +13,5 @@ authRouter.post("/signup", signupController);
 authRouter.post("/login", loginController);
 authRouter.post("/logout", logoutController);
 authRouter.post("/update-profile", protectRoute, updateProfile);
+authRouter.get("/check", protectRoute, checkAuth);
 export default authRouter;

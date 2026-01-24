b78f2a1d793bafee86879a9446f8510b5248180b
Sriram Annadurai <mailsriram98@gmail.com>
2026-01-24 14:27:02 +0530

    Created jwtToken generator in utils, built signupController to authenticate user signups

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,17 +1,61 @@
+import { generateToken } from "../lib/utils.js";
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
 export const signupController = async (req, res) => {
+    const { fullName, email, password } = req.body;
     try {
-        res.send("SignUp route");
-    } catch (error) {}
+        if (password.length < 6) {
+            return res.status(400).json({
+                success: false,
+                message: "Password must be at least 6 characters.",
+            });
+        }
+        const user = await User.findOne({ email });
+        if (user) {
+            return res.status(400).json({
+                success: false,
+                message: "Account with this email id already exists!",
+            });
+        }
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+            fullName,
+            email,
+            password: hashedPassword,
+        });
+        if (newUser) {
+            generateToken(newUser._id, res);
+            await newUser.save();
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                email: newUser.email,
+                profilePic: newUser.profilePic,
+            });
+        } else {
+            res.status(400).json({
+                success: false,
+                message: "Invalid user data",
+            });
+        }
+    } catch (error) {
+        console.log("Error occured in signUp controller", error.message);
+        res.status(500).json({
+            success: false,
+            message: "Error occured while signing up user",
+        });
+    }
 };
 
 export const loginController = async (req, res) => {
     try {
-        res.send("Login route");
     } catch (error) {}
 };
 
 export const logoutController = async (req, res) => {
     try {
-        res.send("Logout route");
     } catch (error) {}
 };
--- backend/src/server.js
+++ backend/src/server.js
@@ -8,6 +8,8 @@ import authRouter from "./routes/auth.router.js";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
 app.use("/api/auth", authRouter);
 
 app.listen(PORT, () => {

604e893bc1278e602b6d0c367c9bd3d9eb9fa0e7
Sriram Annadurai <mailsriram98@gmail.com>
2026-01-24 22:54:45 +0530

    added logic to getMessages method which will handle the GET method for /messages endpoint

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,5 +1,5 @@
-import User from "../models/user.model";
-
+import User from "../models/user.model.js";
+import Message from "../models/message.model.js";
 export const getUsersForSidebar = async (req, res) => {
     try {
         const loggedInUserId = req.user_id;
@@ -9,12 +9,25 @@ export const getUsersForSidebar = async (req, res) => {
 
         res.status(200).json(filteredUsers);
     } catch (error) {
-        console.error("Error in getUsersForSidebar: ", error.message);
+        console.log("Error in getUsersForSidebar: ", error.message);
         res.status(500).json({ error: "Internal server error" });
     }
 };
 
 export const getMessages = async (req, res) => {
     try {
-    } catch (error) {}
+        const { id: userToChatId } = req.params;
+        const myId = req.user._id;
+
+        const messages = await Message.find({
+            $or: [
+                { senderId: myId, recieverId: userToChatId }, // get all the messages I've ever sent to which only has a reciever ID of this mentioned person that I want to chat to
+                { senderId: userToChatId, recieverId: myId }, // get all the messages of the person I want to chat to ever sent, but... not all, just the one's that he sent to me
+            ],
+        });
+        res.status(200).json(messages);
+    } catch (error) {
+        console.log("Error in getMessages router: ", error.message);
+        res.status(500).json({ error: "Internal server error" });
+    }
 };

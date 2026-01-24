45a68e181f0283e5448c9d44322748c031039b68
Sriram Annadurai <mailsriram98@gmail.com>
2026-01-25 01:33:37 +0530

    Cleared boilerplate and created skeletal Page Components for frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,17 @@
-import "./App.css";
+import React from "react";
 
-function App() {
-    return <div>Let's start building!</div>;
-}
+const App = () => {
+    return (
+        <div className="text-red-500">
+            <button class="btn btn-active">Default</button>
+            <button class="btn btn-active btn-neutral">Neutral</button>
+            <button class="btn btn-active btn-primary">Primary</button>
+            <button class="btn btn-active btn-secondary">Secondary</button>
+            <button class="btn btn-active btn-accent">Accent</button>
+            <button class="btn btn-active btn-ghost">Ghost</button>
+            <button class="btn btn-active btn-link">Link</button>
+        </div>
+    );
+};
 
 export default App;

b51bfb98ccfffe2953dba3db2581ce68a3a38c81
Sriram Annadurai <mailsriram98@gmail.com>
2026-01-28 01:27:27 +0530

    wrote code to emmit current online users on login and logout

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -11,11 +11,23 @@ const io = new Server(server, {
     },
 });
 
+const userSocketMap = {}; //{userId: socketId}
+
 io.on("connection", (socket) => {
     console.log("A user connected", socket.id);
 
+    const userId = socket.handshake.query.userId;
+
+    if (userId) {
+        userSocketMap[userId] = socket.id; //{userId: socketId}
+    }
+
+    io.emit("getOnlineUsers", Object.keys(userSocketMap)); // used to send events to all the connected clients/users
+
     socket.on("disconnect", () => {
         console.log("A user disconnected", socket.id);
+        delete userSocketMap[userId]; //deleting the logged out user from the map using userId as key
+        io.emit("getOnlineUsers", Object.keys(userSocketMap));
     });
 });
 

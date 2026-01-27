db0a17673f025088cbf76e283ea26de30adc45bb
Sriram Annadurai <mailsriram98@gmail.com>
2026-01-27 22:53:36 +0530

    Initialized socketio in backend to implement live message update

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,12 +3,12 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import { connectDB } from "./lib/db.js";
-
+import { app, server, io } from "./lib/socket.js";
 import authRouter from "./routes/auth.router.js";
 import messageRouter from "./routes/message.router.js";
 
 dotenv.config();
-const app = express();
+
 const PORT = process.env.PORT || 3000;
 
 app.use(
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRouter);
 app.use("/api/messages", messageRouter);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log("Server is runnin on port", PORT);
     connectDB();
 });
